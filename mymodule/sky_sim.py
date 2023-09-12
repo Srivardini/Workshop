5fc302ba9a37354daffdf67f2d5d7099955fece2
Srivardini <srivardini99@gmail.com>
2023-09-13 00:22:11 +1000

    Newfunc

--- mymodule/sky_sim.py
+++ mymodule/sky_sim.py
@@ -1,30 +1,37 @@
 #! /usr/bin/env python
 
+#Determine Andromeda location in ra/decs
+
 # convert to decimal degrees
+
 from math import cos, pi
 from random import uniform
 
+def clip_to_radius():
+    pass
 
-# from wikipedia
-RA = '00:42:44.3'
-DEC = '41:16:09'
+def generate_sky_pos():
+        
+    # from wikipedia
+    RA = '00:42:44.3'
+    DEC = '41:16:09'
 
-d, m, s = DEC.split(':')
-DEC = int(d)+int(m)/60+float(s)/3600
+    d, m, s = DEC.split(':')
+    DEC = int(d)+int(m)/60+float(s)/3600
 
-h, m, s = RA.split(':')
-RA = 15*(int(h)+int(m)/60+float(s)/3600)
-RA = RA/cos(DEC*pi/180)
+    h, m, s = RA.split(':')
+    RA = 15*(int(h)+int(m)/60+float(s)/3600)
+    RA = RA/cos(DEC*pi/180)
 
-NSRC = 1_000_000
+    NSRC = 1_000_000
 
-# make 1000 stars within 1 degree of Andromeda
+    # make 1000 stars within 1 degree of Andromeda
 
-ras = []
-decs = []
-for i in range(NSRC):
-    ras.append(RA + uniform(-1,1))
-    decs.append(DEC + uniform(-1,1))
+    ras = []
+    decs = []
+    for i in range(NSRC):
+        ras.append(RA + uniform(-1,1))
+        decs.append(DEC + uniform(-1,1))
 
 
 # now write these to a csv file for use by my other program
