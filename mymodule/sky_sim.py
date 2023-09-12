079b5e559ff0fdbf79a1cbecf0074d7ba1322bc3
Srivardini <srivardini99@gmail.com>
2023-09-12 23:32:42 +1000

    initial version

---
+++ mymodule/sky_sim.py
@@ -0,0 +1,34 @@
+#! /usr/bin/env python
+
+# convert to decimal degrees
+from math import cos, pi
+from random import uniform
+
+
+# from wikipedia
+RA = '00:42:44.3'
+DEC = '41:16:09'
+
+d, m, s = DEC.split(':')
+DEC = int(d)+int(m)/60+float(s)/3600
+
+h, m, s = RA.split(':')
+RA = 15*(int(h)+int(m)/60+float(s)/3600)
+RA = RA/cos(DEC*pi/180)
+
+NSRC = 1_000_000
+
+# make 1000 stars within 1 degree of Andromeda
+
+ras = []
+decs = []
+for i in range(NSRC):
+    ras.append(RA + uniform(-1,1))
+    decs.append(DEC + uniform(-1,1))
+
+
+# now write these to a csv file for use by my other program
+f = open('catalog.csv','w')
+print("id,ra,dec", file=f)
+for i in range(NSRC):
+    print("{0:07d}, {1:12f}, {2:12f}".format(i, ras[i], decs[i]), file=f)
