250fb8c5be2f8c38c1f396f4ce39dbded8e36237
Srivardini <srivardini99@gmail.com>
2023-09-13 10:47:44 +1000

    adding argparse

--- mymodule/sky_sim.py
+++ mymodule/sky_sim.py
@@ -1,51 +1,125 @@
 #! /usr/bin/env python
+"""
+Determine Andromeda location in ra/dec degrees
+"""
 
-#Determine Andromeda location in ra/decs
-
-# convert to decimal degrees
-
-from math import cos, pi
 from random import uniform
+from math import cos, sin, pi
 
-NSRC = 1_000_000
 
-def clip_to_radius[(ra, dec, ras, decs)]:
+NSRC = 1_000
+RA = '00:42:44.3'
+DEC = '41:16:09'
+
+import argparse
+
+def skysim_parser():
+    """
+    Configure the argparse for skysim
+
+    Returns
+    -------
+    parser : argparse.ArgumentParser
+        The parser for skysim.
+    """
+    parser = argparse.ArgumentParser(prog='sky_sim', prefix_chars='-')
+    parser.add_argument('--ra', dest = 'ra', type=float, default=None,
+                        help="Central ra (degrees) for the simulation location")
+    parser.add_argument('--dec', dest = 'dec', type=float, default=None,
+                        help="Central dec (degrees) for the simulation location")
+    parser.add_argument('--out', dest='out', type=str, default='catalog.csv',
+                        help='destination for the output catalog')
+    return parser
+
+def get_radec():
+    """
+    Generate the ra/dec coordinates of Andromeda
+    in decimal degrees.
+
+    Returns
+    -------
+    ra : float
+        The RA, in degrees, for Andromeda
+    dec : float
+        The DEC, in degrees for Andromeda
+    """
+    # from wikipedia
+    andromeda_ra = '00:42:44.3'
+    andromeda_dec = '41:16:09'
+
+    d, m, s = andromeda_dec.split(':')
+    dec = int(d)+int(m)/60+float(s)/3600
+
+    h, m, s = andromeda_ra.split(':')
+    ra = 15*(int(h)+int(m)/60+float(s)/3600)
+    ra = ra/cos(dec*pi/180)
+    return ra,dec
+
+def clip_to_radius(ra, dec, ras, decs):
     output_ras = []
     output_decs = []
     for ra_i, dec_i in zip(ras, decs):
-        if ra_i**2 + dec_i**2 <1:
+        if ra_i**2 + dec_i**2 < 1:
             output_ras.append(ra_i)
             output_decs.append(dec_i)
     return output_ras, output_decs
 
-def get_radec():
-    pass
 
 def generate_sky_pos():
-        
     # from wikipedia
-    RA = '00:42:44.3'
-    DEC = '41:16:09'
+    ra = '00:42:44.3'
+    dec = '41:16:09'
 
-    d, m, s = DEC.split(':')
-    DEC = int(d)+int(m)/60+float(s)/3600
+    # convert to decimal degrees
 
-    h, m, s = RA.split(':')
-    RA = 15*(int(h)+int(m)/60+float(s)/3600)
-    RA = RA/cos(DEC*pi/180)
+    d, m, s = dec.split(':')
+    dec = int(d)+int(m)/60+float(s)/3600
 
-    
+    h, m, s = ra.split(':')
+    ra = 15*(int(h)+int(m)/60+float(s)/3600)
+    ra = ra/cos(dec*pi/180)
+
 
     # make 1000 stars within 1 degree of Andromeda
     ras = []
     decs = []
     for i in range(NSRC):
-        ras.append(RA + uniform(-1,1))
-        decs.append(DEC + uniform(-1,1))
+        ras.append(ra + uniform(-1,1))
+        decs.append(dec + uniform(-1,1))
+    return ras, decs
 
 
-# now write these to a csv file for use by my other program
+def make_stars(ra, dec, nsrc=NSRC):
+    """
+    Generate NSRC stars within 1 degree of the given ra/dec
+
+    Parameters
+    ----------
+    ra,dec : float
+        The ra and dec in degrees for the central location.
+    nsrc : int
+        The number of star locations to generate
+
+    Returns
+    -------
+    ras, decs : list
+        A list of ra and dec coordinates.
+    """
+    ras = []
+    decs = []
+    for _ in range(nsrc):
+        ras.append(ra + random.uniform(-1,1))
+        decs.append(dec + random.uniform(-1,1))
+    return ras, decs
+
+
+def main():
+    ra_deg, dec_deg = get_radec()
+    ras, decs = generate_sky_pos()
+    ras, decs = clip_to_radius(ras, decs)
+
+    # now write these to a csv file for use by my other program
     with open('catalog.csv','w') as f:
         print("id,ra,dec", file=f)
         for i in range(NSRC):
-            print("{0:07d}, {1:12f}, {2:12f}".format(i, ras[i], decs[i]), file=f)
+            print(f"{i:07d}, {ras[i]:12f}, {decs[i]:12f}", file=f)
