edd683c01982349323b72493bd8c4a91e9941e60
Srivardini <srivardini99@gmail.com>
2023-09-13 11:41:36 +1000

    extension adding

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,15 @@ release = '1.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.doctest',
+    'sphinx.ext.mathjax',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.githubpages',
+    'numpydoc',
+    'myst_parser',
+]
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -24,5 +32,6 @@ language = 'yes'
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
+
 html_static_path = ['_static']
