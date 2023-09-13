268fc5af73748ee56cb9a6ab8771b0d78c94a5bf
Srivardini <srivardini99@gmail.com>
2023-09-13 11:10:03 +1000

    adding functions

--- tests/test_sky_sim.py
+++ tests/test_sky_sim.py
@@ -1,5 +1,7 @@
 import pytest
+
 from mymodule.sky_sim import get_radec
+#, segadecimal_to_float, float_to_segadecimal
 
 def test_module_import():
     try:
@@ -10,9 +12,30 @@ def test_module_import():
 
 def test_get_radec_values():
     """
-    This  checks that get radec gives back the correct values 
-    of Andromeda in decimal degrees
+    This checks that get_radec gives back the correct values of
+    Andromida in decimal degrees
     """
     ra, dec = get_radec()
-    assert ra == pytest.approx(14.215420962967535)
-    assert dec == pytest.approx(41.269166666666667)
+    assert ra == pytest.approx(14.215420962967535, rel=10e-30)
+    assert dec == pytest.approx(41.26916666666667)
+
+@pytest.mark.parametrize("ra,dec", [
+    (14.215420962967535, 41.26916666666667),
+    (4.215420962967535, 4.26916666666667),
+    (4.215420962967535, 4.26916666666667),
+])
+def test_segadecimal_to_float(ra, dec):
+    print(ra, dec)
+    #new_ra, new_dec = segadecimal_to_float(
+    #    *float_to_segadecimal(ra, dec)
+    #)
+    #assert new_ra == pytest.approx(ra)
+    #assert new_dec == pytest.approx(dec)
+
+# @pytest.mark.parametrize("input,output", [
+#     (14.215420962967535, 41.26916666666667),
+#     (4.215420962967535, 4.26916666666667),
+#     (4.215420962967535, 4.26916666666667),
+# ])
+# def test_magic_function(input, output):
+#     assert magic(input) == output
